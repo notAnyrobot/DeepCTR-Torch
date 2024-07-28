d023377f2e6afffe3991a0af743fbb8b9b1728e0
notAnyrobot <liao.shiwen@qq.com>
2024-07-28 20:06:17 +0800

    fixes examples path issue

--- examples/run_classification_criteo.py
+++ examples/run_classification_criteo.py
@@ -1,22 +1,31 @@
 # -*- coding: utf-8 -*-
+import os
+
 import pandas as pd
 import torch
 from sklearn.metrics import log_loss, roc_auc_score
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 
-from deepctr_torch.inputs import SparseFeat, DenseFeat, get_feature_names
+from deepctr_torch.inputs import DenseFeat, SparseFeat, get_feature_names
 from deepctr_torch.models import *
 
+EXAMPLES_PATH = os.path.dirname(os.path.abspath(__file__))
+
 if __name__ == "__main__":
-    data = pd.read_csv('./criteo_sample.txt')
+    data = pd.read_csv(os.path.join(EXAMPLES_PATH, "./criteo_sample.txt"))
+    # data = pd.read_csv("./criteo_sample.txt")
 
-    sparse_features = ['C' + str(i) for i in range(1, 27)]
-    dense_features = ['I' + str(i) for i in range(1, 14)]
+    sparse_features = ["C" + str(i) for i in range(1, 27)]
+    dense_features = ["I" + str(i) for i in range(1, 14)]
 
-    data[sparse_features] = data[sparse_features].fillna('-1', )
-    data[dense_features] = data[dense_features].fillna(0, )
-    target = ['label']
+    data[sparse_features] = data[sparse_features].fillna(
+        "-1",
+    )
+    data[dense_features] = data[dense_features].fillna(
+        0,
+    )
+    target = ["label"]
 
     # 1.Label Encoding for sparse features,and do simple Transformation for dense features
     for feat in sparse_features:
@@ -27,15 +36,23 @@ if __name__ == "__main__":
 
     # 2.count #unique features for each sparse field,and record dense feature field name
 
-    fixlen_feature_columns = [SparseFeat(feat, vocabulary_size=data[feat].max() + 1, embedding_dim=4)
-                              for feat in sparse_features] + [DenseFeat(feat, 1, )
-                                                              for feat in dense_features]
+    fixlen_feature_columns = [
+        SparseFeat(feat, vocabulary_size=data[feat].max() + 1, embedding_dim=4)
+        for feat in sparse_features
+    ] + [
+        DenseFeat(
+            feat,
+            1,
+        )
+        for feat in dense_features
+    ]
 
     dnn_feature_columns = fixlen_feature_columns
     linear_feature_columns = fixlen_feature_columns
 
     feature_names = get_feature_names(
-        linear_feature_columns + dnn_feature_columns)
+        linear_feature_columns + dnn_feature_columns
+    )
 
     # 3.generate input data for model
 
@@ -45,21 +62,34 @@ if __name__ == "__main__":
 
     # 4.Define Model,train,predict and evaluate
 
-    device = 'cpu'
+    device = "cpu"
     use_cuda = True
     if use_cuda and torch.cuda.is_available():
-        print('cuda ready...')
-        device = 'cuda:0'
+        print("cuda ready...")
+        device = "cuda:0"
 
-    model = DeepFM(linear_feature_columns=linear_feature_columns, dnn_feature_columns=dnn_feature_columns,
-                   task='binary',
-                   l2_reg_embedding=1e-5, device=device)
+    model = DeepFM(
+        linear_feature_columns=linear_feature_columns,
+        dnn_feature_columns=dnn_feature_columns,
+        task="binary",
+        l2_reg_embedding=1e-5,
+        device=device,
+    )
 
-    model.compile("adagrad", "binary_crossentropy",
-                  metrics=["binary_crossentropy", "auc"], )
+    model.compile(
+        "adagrad",
+        "binary_crossentropy",
+        metrics=["binary_crossentropy", "auc"],
+    )
 
-    history = model.fit(train_model_input, train[target].values, batch_size=32, epochs=10, verbose=2,
-                        validation_split=0.2)
+    history = model.fit(
+        train_model_input,
+        train[target].values,
+        batch_size=32,
+        epochs=10,
+        verbose=2,
+        validation_split=0.2,
+    )
     pred_ans = model.predict(test_model_input, 256)
     print("")
     print("test LogLoss", round(log_loss(test[target].values, pred_ans), 4))
